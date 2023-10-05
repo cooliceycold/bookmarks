5947a8939726d65327152cdf2bc16d2ffb9079bb
cooliceycold <112804160+cooliceycold@users.noreply.github.com>
2023-10-05 17:00:29 +0800

    Update autoload.js

--- jsdelivr/random/autoload.js
+++ jsdelivr/random/autoload.js
@@ -36,7 +36,7 @@ if (screen.width >= 768) {
       waifuPath: live2d_path + "waifu-tips.json",
 
       // apiPath: "https://live2d.fghrsh.net/api/",
-      cdnPath: "https://preeminent-puppy-319710.netlify.app/",
+      cdnPath: "https://jsd.cdn.zzko.cn/gh/cooliceycold/live@latest/",
 
       tools: [
         "hitokoto",
